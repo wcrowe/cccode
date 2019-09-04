c4270484c5d72c1c36e545baeb38edc763c3f1d2
wcrowe <crowe.will@gmail.com>
2019-09-04 07:10:42 -0400

    changing semantic-ui-react to  materila-ui

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,69 +1,59 @@
 import React from 'react';
 import { GlobalHooksProvider, createGlobalHook, useGlobalHook } from '@devhammed/use-global-hook'
-import {TextField} from 'material-ui/TextField';
-
-// class AddUser extends React.Component {
-//     inputRefName = React.createRef();
-//     inputRefCity = React.createRef();
-//     inputRefDateCreate = React.createRef();
-
-//     handleClick = addTodo => {
-//         if (this.inputRefName.current.value) {
-//             addTodo({username :this.inputRefName.current.value, usercity:this.inputRefCity.current.value, userceated: this.inputRefDateCreate.current.value});
-//             this.inputRefName.current.value = '';
-//             this.inputRefCity.current.value = '';            
-//             this.inputRefDateCreate.current.value = '';
-//         }
-//     };
-//     render() {
-//       return (
-//         <div>
-//           <input type="text" placeholder="User Name" ref={this.inputRefName} />
-//           <input type="text" placeholder="Select City" ref={this.inputRefCity} />
-//           <input type="text" placeholder="Pick CreateDate" ref={this.inputRefDateCreate} />
-  
-//           <Subscribe to={[UserContainer]}>
-//             {userContainer => (
-//               <button onClick={() => this.handleClick(userContainer.A)}>
-//                 Add
-//               </button>
-//             )}
-//           </Subscribe>
-//         </div>
-//       );
-//     }
-//   }
-
 import userStateHook from './store';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+
+
 function AddUser() {
+  const { users, loading, addUser, delUser } = useGlobalHook('userStore');
 
-  const { users, loading, addUser, delUser} = useGlobalHook('userStore');
+
+  const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+    },
+    dense: {
+      marginTop: theme.spacing(2),
+    },
+    menu: {
+      width: 200,
+    },
+  }));
+
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
   console.log(users)
-  const handleSubmit =() =>{
+  const handleSubmit = () => {
     console.log(users)
-    addUser('John Doe','Clearwater');
+    addUser('John Doe', 'Clearwater');
+  }
 
-}
- 
-  
-
-  //
- 
   return (
-
-            <div>
-
-              {/* <form method="POST" >
-                <input type="test" name="username"> </input>
-                  <input type="test" name="usercity"></input>
-                  
-              </form> */}
-              <button type="submit" onClick={handleSubmit}>Submit</button>
-            </div>
-            
-
+    <form className={classes.container} noValidate autoComplete="off">
+           <TextField
+        required
+        id="outlined-required"
+        label="Required"
+        defaultValue="Hello World"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+        onChange={handleChange('name')}
+      />
+ 
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
   )
-            }
+}
 export default AddUser;
 
 
