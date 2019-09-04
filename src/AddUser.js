d42545b691dbb9761a64ecdacc599015886017c2
wcrowe <crowe.will@gmail.com>
2019-09-04 10:38:39 -0400

    ants

--- src/AddUser.js
+++ src/AddUser.js
@@ -1,56 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GlobalHooksProvider, createGlobalHook, useGlobalHook } from '@devhammed/use-global-hook'
 import userStateHook from './store';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {
+  Form,
+  Input,
+  Tooltip,
+  Icon,
+  Cascader,
+  Select,
+  Row,
+  Col,
+  Checkbox,
+  Button,
+  AutoComplete,
+} from 'antd';
 
 
 function AddUser() {
   const { users, loading, addUser, delUser } = useGlobalHook('userStore');
 
 
-  const useStyles = makeStyles(theme => ({
+  const styles = theme => ({
     container: {
       display: 'flex',
       flexWrap: 'wrap',
     },
     textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
+      marginLeft: 1,
+      marginRight: 1,
     },
     dense: {
-      marginTop: theme.spacing(2),
+      marginTop: 2,
     },
     menu: {
       width: 200,
     },
-  }));
-
-  const handleChange = name => event => {
-    setValues({ ...values, [name]: event.target.value });
-  };
+    button: {
+      margin: 1,
+    },
+  })
+  
+  // const handleChange = name => event => {
+  //   setValues({ ...values, [name]: event.target.value });
+  // };
 
   console.log(users)
-  const handleSubmit = () => {
-    console.log(users)
-    addUser('John Doe', 'Clearwater');
-  }
+
+  //const classes = useStyles();
+  // const [values, setValues] = React.useState({});
+
+  // function handleChange(e) {
+  
+  //   e.setValues(e.target.values);
+  // }
 
   return (
-    <form className={classes.container} noValidate autoComplete="off">
-           <TextField
+    <form  noValidate autoComplete="off">
+      <TextField
         required
         id="outlined-required"
         label="Required"
         defaultValue="Hello World"
-        className={classes.textField}
         margin="normal"
         variant="outlined"
-        onChange={handleChange('name')}
+      //  onChange={handleChange('name')}
       />
- 
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+
+      <Button variant="contained" color="primary" >
+        Submit
+      </Button>
     </form>
   )
 }
